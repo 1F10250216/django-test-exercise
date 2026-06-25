e7e3bffd8b01c16c455ccfcadf391994513c96bd
1F10250216 <s1F102502160@iniad.org>
2026-06-25 14:01:06 +0900

    修正

--- todo/tests.py
+++ todo/tests.py
@@ -1,12 +1,15 @@
-from django.test import TestCase,Client
+from django.test import TestCase, Client
 from django.utils import timezone
 from datetime import datetime
 from todo.models import Task
 
 # Create your tests here.
+
+
 class SampleTestCase(TestCase):
     def test_sample(self):
         self.assertEqual(1+2, 3)
+
 
 class TaskModelTestCase(TestCase):
     def test_create_task1(self):
@@ -51,21 +54,22 @@ class TaskModelTestCase(TestCase):
 
         self.assertFalse(task.is_overdue(current))
 
+
 class TodoViewTestCase(TestCase):
     def test_index_get(self):
         client = Client()
         response = client.get('/')
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.templates[0].name,'todo/index.html')
+        self.assertEqual(response.templates[0].name, 'todo/index.html')
         self.assertEqual(len(response.context['tasks']), 0)
 
     def test_index_post(self):
         client = Client()
-        data = {'title':'Test Task', 'due_at':'2024-06-30 23:59:59'}
+        data = {'title':'Test Task', 'due_at': '2024-06-30 23:59:59'}
         response = client.post('/', data)
 
-        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/index.html')
         self.assertEqual(len(response.context['tasks']), 1)
 
@@ -94,3 +98,4 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.templates[0].name, 'todo/index.html')
         self.assertEqual(response.context['tasks'][0], task1)
         self.assertEqual(response.context['tasks'][1], task2)
+        
